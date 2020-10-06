41d3e59435b04da80b71418811694b56e4f67e7a
David Kuyk <72398435+davidkuyk@users.noreply.github.com>
2020-10-06 12:40:24 -0500

    Add main files to upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include 'php/header.php';?>
+
+<!-- BANNER -->
+	<div class="banner">
+    <h2><span class="yellow">TUTORING</span> SIMPLIFIED.</h2>
+  </div>
+
+<!-- PICTURE -->
+<div class="main-photo">
+  <img src="images/masterminds-tutoring-birmingham-al.png" alt="A tutor tutoring a Birmingham student" />
+</div>
+
+<!-- DESCRIPTION -->
+<div class="about">
+  <h1>Masterminds Tutoring has provided one-on-one tutoring in Birmingham, AL to students of all ages for 20 years. We're Birmingham’s only locally owned in-home tutoring company.</h1>
+  <h1>We keep it simple. Our handpicked tutors focus on your student's immediate struggles and work from there. You choose the tutor, time, and meeting place.</h1>
+  <h1>Our tutors can help with most K-12 subjects, including: elementary math, algebra 1, algebra 2 (with trigonometry), pre-algebra, geometry, pre-calculus, calculus, statistics, physical science, biology, chemistry, physics, english, history, government/economics, Spanish, French, German, homework help, and ACT or SAT test prep.</h1>
+  <h1>We serve most of Jefferson and Shelby County, including Vestavia, Hoover, Mountain Brook, Homewood, Cahaba Heights, Liberty Park, Inverness, Greystone, Meadowbrook and the Highway 280 area.</h1>
+</div>
+<br>
+<?php include 'php/footer.php';?>
---
+++ php/header.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <link type="text/css" rel="stylesheet" href="/css/style.css?<?php echo time(); ?>" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=EB+Garamond:400,700" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-64487144-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      /* gtag for google analytics */
+      gtag('config', 'UA-64487144-1');
+      /* gtag for google adwords conversions */
+      gtag('config', 'AW-942286034');
+    </script>
+
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>Masterminds Tutoring</title>
+    <meta name=viewport content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+    <script type="text/javascript">var switchTo5x=true;</script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" defer></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.21/jquery-ui.min.js" defer></script>
+
+    <meta name="description" content="Masterminds Tutoring provides convenient, individualized tutoring services to Birmingham middle and high school students in all 6th through 12th grade subjects." />  
+  </head>
+
+  <body>
+    <div class="header">
+        <div class="content">
+            <!---<div class="alert">
+                <h3>Attention: Masterminds will be temporarily closed for summer vacation during the month of June. We'll be back in operation July 1st. Feel free to email us in June, and we'll respond on July 1st. We hope you have a wonderful summer vacation!</h3>
+            </div>-->
+            <div class="logo">
+                <a href="index.php"><span><img src="images/masterminds-tutoring-birmingham-al-logo.png" alt="Masterminds Tutoring Birmingham logo"/></span><h1>Masterminds Tutoring</h1></a>
+            </div>
+            <ul id="menu-main-menu" class="menu">
+                <li class="menu-item"><a href="testimonials.php">Testimonials</a></li>
+                <li class="menu-item"><a href="tutoring.php">Tutoring</a></li>
+                <li class="menu-item"><a href="tutors.php">Tutors</a></li>
+                <li class="menu-item"><a href="contact.php">Contact</a></li>
+                <li class="menu-item"><a href="payments.php">Payments</a></li>
+            </ul>
+        </div>
+    </div>
+    <a id="schedulenow" onclick="goog_report_conversion ('/schedule')" href="schedule.php"><strong>Schedule Now</strong></a>
