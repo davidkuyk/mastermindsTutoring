1933a21181b430071bfe003baa06f5c251579bfc
David Kuyk <72398435+davidkuyk@users.noreply.github.com>
2020-10-08 15:49:43 -0500

    Add message to homepage about permanent closure

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 
 <!-- DESCRIPTION -->
 <div class="about">
+  <h1 style="color:red;">Masterminds Tutoring is permanently closed. This site is for archival purposes only.</h1>
   <h1>Masterminds Tutoring has provided one-on-one tutoring in Birmingham, AL to students of all ages for 20 years. We're Birmingham’s only locally owned in-home tutoring company.</h1>
   <h1>We keep it simple. Our handpicked tutors focus on your student's immediate struggles and work from there. You choose the tutor, time, and meeting place.</h1>
   <h1>Our tutors can help with most K-12 subjects, including: elementary math, algebra 1, algebra 2 (with trigonometry), pre-algebra, geometry, pre-calculus, calculus, statistics, physical science, biology, chemistry, physics, english, history, government/economics, Spanish, French, German, homework help, and ACT or SAT test prep.</h1>
